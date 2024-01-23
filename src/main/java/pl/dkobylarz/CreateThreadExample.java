6fffb4b66dccaa3dbc9874e7f9038513d2ce93a9
Dawid Kobylarz <dawid.kobylarz@protonmail.com>
2024-01-23 20:23:40 +0100

    Create thread example

--- src/main/java/pl/dkobylarz/CreateThreadExample.java
+++ src/main/java/pl/dkobylarz/CreateThreadExample.java
@@ -1,16 +1,16 @@
 package pl.dkobylarz;
 
-public class Main {
+public class CreateThreadExample {
     public static void main(String[] args) {
         System.out.println("I'm Inside: " + Thread.currentThread());
         Thread thread = new ThreadExample();
         thread.start();
     }
-}
 
-class ThreadExample extends Thread{
-    @Override
-    public void run() {
-        System.out.println("I'm Inside: " + Thread.currentThread().threadId());
+    static class ThreadExample extends Thread{
+        @Override
+        public void run() {
+            System.out.println("I'm Inside: " + Thread.currentThread().getName());
+        }
     }
 }
